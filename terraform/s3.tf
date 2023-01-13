4f32f75ea8af61ffefc1ecc0c4ec4cdce73fb26e
Robert Smith <rbrtsmith84@googlemail.com>
2023-01-13 13:42:55 +0000

    add bucket acl resource

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,10 +1,14 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "rws-microfrontend-bucket"
-  acl = "public-read"
 
   tags = {
     Name  = "Microfrontend Bucket"
   }
+}
+
+resource "aws_s3_bucket_acl" "website_bucket_acl" {
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "website_bucket_config" {
