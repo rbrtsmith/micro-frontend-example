bc754c155c811745c9056ad43c3a02776e8b557b
Robert Smith <rbrtsmith84@googlemail.com>
2023-01-13 11:41:40 +0000

    test removing required providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      # Version = "~>3.27"
-    }
-  }
+  # required_providers {
+  #   aws = {
+  #     source = "hashicorp/aws"
+  #     # Version = "~>3.27"
+  #   }
+  # }
 
   required_version = ">=1.3.7"
   backend "s3" {
