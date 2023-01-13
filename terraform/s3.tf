655df9ee99a1ef5cf3240e7ad298e770acc2b06c
Robert Smith <rbrtsmith84@googlemail.com>
2023-01-13 11:26:46 +0000

    test

---
+++ terraform/s3.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "rws-microfrontend-bucket"
+  acl = "public-read"
+
+  tags = {
+    Name  = "Microfrontend Bucket"
+  }
+
+  policy = <<EOF
+    "id": "MakePublic",
+    "version": "2012-10-17",
+    "statement": [
+      {
+        "action": [
+          "s3:GetObject"
+        ],
+        "effect": "Allow",
+        "resource": "arn:aws:s3:::rws-microfrontend-bucket/*",
+        "principal": "*"
+      }
+    ]
+    EOF
+  
+  website {
+    index_document = "index.html"
+  }
+}
