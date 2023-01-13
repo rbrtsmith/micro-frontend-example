60552dfa34caebe1595c0e21e18397352a2924b1
Robert Smith <rbrtsmith84@googlemail.com>
2023-01-13 15:23:05 +0000

    fix index document

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -24,7 +24,14 @@ resource "aws_s3_bucket_acl" "website_bucket_acl" {
   bucket = aws_s3_bucket.website_bucket.id
   acl    = "public-read"
 }
-
 resource "aws_s3_bucket_website_configuration" "website_bucket_config" {
   bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    suffix = "index.htmll"
+  }
 }
+
